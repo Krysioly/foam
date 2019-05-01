26ee36cfb25f409ef7748ee2120663b5339c6c1f
krysioly <krysioly@gmail.com>
2019-05-01 21:30:23 +0000

    props to wood

--- model.py
+++ model.py
@@ -3,14 +3,31 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Props():
+class Wood(db.Model):
+    """ Create woods in database"""
 
-    __tablename__ = "props"
+    __tablename__ = "woods"
 
-    name = db.Column(db.String(100), nullable = False)
-    image = db.Column(db.Sting(100), nullable = False)
+    name = db.Column(db.String(100), nullable = False, primary_key= True)
+    image = db.Column(db.String(100), nullable = False)
 
 
     def __repr__(self):
             """Provide helpful representation when printed."""
-            return f"<Prop name={self.name}>"
+            return f"<Wood name={self.name}>"
+
+############################################################################
+def connect_to_db(app, db_uri='postgresql:///woods'):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import (Flask, request, render_template, redirect, session)
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
+from model import connect_to_db, db, Wood
 
 app = Flask(__name__)
-app.secret_key = "papabear"
+app.secret_key = "yes"
 app.jinja_env.undefined = StrictUndefined
 
 
@@ -11,13 +12,33 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     # show homepage
     print("\n\nshow homepage\n\n")
+    
     return render_template("homepage.html")
      
+@app.route('/woods')
+def show_woods():
+    """Show woods"""
+    findwoods = Wood.query.all()
+    return render_template("woods.html", woods = findwoods)
+     
+@app.route('/tables')
+def show_tables():
+    """Show tables"""
+    return render_template("tables.html")
+     
+@app.route('/smalls')
+def show_smalls():
+    """Show smalls"""
+    return render_template("smalls.html")
+     
+
+
+
 
 if __name__ == "__main__":
     app.debug = True
     app.jinja_env.auto_reload = app.debug
     DebugToolbarExtension(app)
 
-    # connect_to_db(app, 'postgresql:///journals')
+    connect_to_db(app, 'postgresql:///woods')
     app.run(port=5000, host='0.0.0.0')
